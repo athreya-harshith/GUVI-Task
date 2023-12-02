64515b823f52a57466cf75fc55105224a1d7141b
athreya-harshith <athreya1207@gmail.com>
2023-12-02 17:11:13 +0530

    FEAT : Added scripts

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const userPasswordEl = document.querySelector("#password");
+const togglePasswordEl = document.querySelector("#togglePassword");
+togglePasswordEl.addEventListener("click", function () {
+  if (this.checked === true) {
+    userPasswordEl.setAttribute("type", "text");
+  } else {
+    userPasswordEl.setAttribute("type", "password");
+  }
+});
